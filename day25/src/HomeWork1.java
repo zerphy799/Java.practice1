e9953c79c97e176404cb759d7000d7e1f84548dc
zerphy799 <2692787472@qq.com>
2026-01-28 22:47:12 +0800

    1

--- day25/src/HomeWork1.java
+++ day25/src/HomeWork1.java
@@ -2,11 +2,12 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class HomeWork1 {
+    static int sum=0;
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         FutureTask<Integer> f1 = new FutureTask<Integer>(() -> {
-            int sum=0;
-            for (int i = 0; i < 500; i++) {
+
+            for (int i = 1; i <= 500; i++) {
                 sum += i;
             }return sum;
         });
@@ -15,7 +16,7 @@ public class HomeWork1 {
         FutureTask<Integer> f2 =new  FutureTask<Integer>(()->{
             Thread.sleep(1200);
             int sum=f1.get();
-            for (int i = 0; i < 500; i++) {
+            for (int i = 501; i <= 1000; i++) {
                 sum+=i;
             }
             return sum;
--- day25/src/HomeWork2.java
+++ day25/src/HomeWork2.java
@@ -1,39 +1,44 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 public class HomeWork2 {
-    public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(()->{
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        FutureTask<Long> task1=new FutureTask<Long>(()-> {
+            long l = System.currentTimeMillis();
             int i=0;
             while(i<=100){
-              int a=(int)(Math.random()*2+1);
+                int a=(int)(Math.random()*2+1);
                 i+=a;
             }
+            long l1 = System.currentTimeMillis();
+            return l1-l;
         });
-        t1.start();
-        Thread t2 = new Thread(()->{
-            int j=0;
+        new Thread(task1).start();
+       FutureTask<Long> task2=new FutureTask<>(()->{
+           long l = System.currentTimeMillis();
+           int j=0;
             while(j<=100){
-                int a=(int)(Math.random()*2+3);
-                j+=a;
-                if(j>=70){
-                    try {
-                        Thread.sleep(2);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        });
-        t2.start();
+               int a=(int)(Math.random()*2+3);
+               j+=a;
+               if(j>=70){
+                   try {
+                       Thread.sleep(2);
+                   } catch (InterruptedException e) {
+                       throw new RuntimeException(e);
+                   }
+               }
+           }
+            long l1 = System.currentTimeMillis();
+            return l1-l;
+       });
+        new Thread(task2).start();
         Thread.sleep(5);
-       if(t1.isAlive()){
-           if(!t2.isAlive()){
-               System.out.println("兔子获胜");
-           }
-       }
-       if(t2.isAlive()){
-           if(!t1.isAlive()){
-               System.out.println("乌龟获胜");
-           }
-       }
+          if(task1.get()>task2.get()) {
+              System.out.println("兔子获胜");
+          }
+           else {
+              System.out.println("乌龟获胜");
+          }
 
     }
 
